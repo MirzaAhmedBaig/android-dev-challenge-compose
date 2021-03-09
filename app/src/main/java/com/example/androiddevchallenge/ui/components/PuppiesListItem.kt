4abfa2b616719ccaaa4fc514068ab9c37bd964df
mirza <mirzaahmed65@gmail.com>
2021-03-09 17:02:36 +0530

    removed wildcard imports

--- app/src/main/java/com/example/androiddevchallenge/ui/components/PuppiesListItem.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/components/PuppiesListItem.kt
@@ -1,7 +1,28 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -13,8 +34,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.MyApp
 import com.example.androiddevchallenge.data.models.PuppyInfo
-import com.example.androiddevchallenge.data.repository.DataRepository
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 @Composable
@@ -48,7 +69,7 @@ fun PuppiesListItem(puppyInfo: PuppyInfo, modifier: Modifier = Modifier) {
 @Composable
 fun LightPreview() {
     MyTheme {
-        PuppiesListItem(puppyInfo = DataRepository.puppiesInfoList.first())
+        MyApp()
     }
 }
 
@@ -56,6 +77,6 @@ fun LightPreview() {
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
-        PuppiesListItem(puppyInfo = DataRepository.puppiesInfoList.first())
+        MyApp()
     }
 }
